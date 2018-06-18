b59f3a2b8f9aaf2d36ac3cb5147773a6af8d5c2a
Derrick Kipchumba <dkip64@gmail.com>
2018-06-18 14:21:07 +0300

    [ft #158423893] add configuration file

---
+++ API/app/config.py
@@ -0,0 +1,23 @@
+
+class Config():
+    """Base configuration class."""
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+
+class TestingConfig(Config):
+    """Configurations for Testing."""
+    TESTING = True
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+}
