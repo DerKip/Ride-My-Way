585f0e386ae6d77b494c5173311dde17bfe4b8ab
Derrick Kipchumba <dkip64@gmail.com>
2018-06-21 02:56:19 +0300

    [ft #158489868] update models

--- API/models/driver.py
+++ API/models/driver.py
@@ -1,13 +1,31 @@
 from models.user import UserData
 
-drivers = []
+drivers = [{
+                "id":1,
+                "username":"Tabby",
+                "email":"tabby@gmail.com",
+                "car_model":"BMW",
+                "car_regno":"KBG 009T",
+                "password":"tabbytabbz",    
+                },
+                {
+                "id":2,
+                "username":"John",
+                "email":"johnyy@gmail.com",
+                "car_model":"BENZ",
+                "car_regno":"KCG 909T",
+                "password":"yulemse",    
+                }]
 
 class DriverData(UserData):
     """This class stores drivers data"""
 
     def __init__(self, username,email,password,car_model,car_regno):
         super().__init__(username,email,password)
+        self.id = len (drivers)+ 1
         self.car_model = car_model
         self.car_regno = car_regno
+
+
 
     
--- API/models/user.py
+++ API/models/user.py
@@ -3,9 +3,9 @@ class UserData():
     """This class stores user data"""
 
     def __init__(self, username,email,password):
+        self.id = len (users)+ 1
         self.username = username
         self.email = email
         self.password = password
 
-    
     
