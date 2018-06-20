53622d1b73fab94c48f826b4a6a5500cd9b9a10d
Derrick Kipchumba <dkip64@gmail.com>
2018-06-20 20:08:01 +0300

    [ft #158489868] create user model class

---
+++ API/models/user.py
@@ -0,0 +1,11 @@
+
+class UserData():
+    """This class stores user data"""
+
+    def __init__(self, username,email,password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    users=[]
+    
