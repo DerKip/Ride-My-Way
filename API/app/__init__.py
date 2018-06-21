9c1c1c768976ad04d8de1ae255e6f70c943b7818
Derrick Kipchumba <dkip64@gmail.com>
2018-06-22 02:11:22 +0300

    [ft #158523680] register user_driver blueprint

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from app.config import app_config
 from routes.driver import driver_route
-from routes.user import user_route
+from routes.user import user_route ,user_driver_route
+
 
 def initialize_app(config_name="development"):
     """initializing app in development mode"""
@@ -9,4 +10,6 @@ def initialize_app(config_name="development"):
     app.config.from_object(app_config[config_name])
     app.register_blueprint(driver_route, url_prefix="/api/v1/driver")
     app.register_blueprint(user_route,url_prefix="/api/v1/user")
+    app.register_blueprint(user_driver_route,url_prefix="/api/v1")
+    app.config['SECRET_KEY'] = 'thisIsMySecrectKey'
     return app
--- API/routes/user.py
+++ API/routes/user.py
@@ -1,19 +1,35 @@
-from flask import Blueprint, jsonify, request, Response
-from app.controllers import registration_controller
+from flask import Blueprint, jsonify, request
+from app.controllers import registration_controller, login_controller
 from models.rides import all_ride_offers
 from utils import JSON_MIME_TYPE, json_response
+import datetime
+
 
 user_route = Blueprint("route_user",__name__)
+user_driver_route = Blueprint("route_user_driver",__name__)
 
 @user_route.route('/register', methods=['POST'])
 def register_user():
     """user registration endpoint"""
-
     if request.content_type != JSON_MIME_TYPE:
         error = jsonify({'error': 'Invalid Content Type'})
         return json_response(error, 400)
     return registration_controller.register_new_user()
 
+@user_driver_route.route('/login', methods=['POST'])
+def login_user():
+    """login user endpoint"""
+    if request.content_type != JSON_MIME_TYPE:
+        error = jsonify({'error': 'Invalid Content Type'})
+        return json_response(error, 400)
+    return login_controller.login()
+
+
+
+        # token = jwt.encode({'user':auth.username, 'exp':datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},)
+    error = jsonify({'error':'Could not verify!'})
+    return json_response(error,401,{'WWW-Authenticate':'Basic realm="Login Required"'})
+    
 @user_route.route('/rides', methods=['GET'])
 def get_all_ride_offers():
     """GET all ride offers endpoint"""
@@ -26,5 +42,3 @@ def get_single_ride_offer(id):
     single_ride_offer = [offer for offer in all_ride_offers if offer['id'] == id]
     return jsonify({"Your Ride Offer:":single_ride_offer}),200
 
-
-    
