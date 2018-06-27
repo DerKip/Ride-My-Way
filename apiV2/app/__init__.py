4df95b8f7c21f05e9d33aa77d0bfac4da3e825d8
Derrick Kipchumba <dkip64@gmail.com>
2018-06-28 01:55:42 +0300

    [finishes #158668698 ch]  create database and db connect method

--- apiV2/app/__init__.py
+++ apiV2/app/__init__.py
@@ -1,15 +1,14 @@
 from flask import Flask
 from config import app_config
-from ..routes.driver import driver_route 
-from ..routes.user import user_route ,user_driver_route
+from apiV2.database import Database
 
+db =  Database()
 
 def initialize_app(config_name="development"):
     """initializing app in development mode"""
     app = Flask(__name__, instance_relative_config=True) #creating our WSGI APP Object
     app.config.from_object(app_config[config_name])
-    app.register_blueprint(driver_route, url_prefix="/api/v2/driver")
-    app.register_blueprint(user_route,url_prefix="/api/v2/user")
-    app.register_blueprint(user_driver_route,url_prefix="/api/v2")
-    app.config['SECRET_KEY'] = 'thisIsMySecrectKey'
+    db.init_app(app)
     return app
+
+    
