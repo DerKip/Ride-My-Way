f647e2018c94d1b2533d56ad5a65d5c3eb2ff69a
Derrick Kipchumba <dkip64@gmail.com>
2018-06-28 01:54:35 +0300

    [ch #158668698] Update config file variable and add a .venv file

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
+import os 
+from dotenv import load_dotenv
+from pathlib import Path   
 
+env_path = Path('.') / '.env' # explicitly providing path to '.env'
+load_dotenv(dotenv_path=env_path)
+#Reads the key,value pair from .env and adds them to environment variables
 class Config():
     """Base configuration class."""
-    DEBUG = False
+    DEBUG = False   
+    DATABASE_NAME = os.getenv("DATABASE_NAME")
+    DATABASE_USER = os.getenv("DATABASE_USER")
+    DATABASE_PASSWORD= os.getenv("DATABASE_PASSWORD")
+    DATABASE_HOST= os.getenv("DATABASE_HOST")
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
