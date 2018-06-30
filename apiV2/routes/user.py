b674d773b810bfdf37ebd123920c2bdd23313150
Derrick Kipchumba <dkip64@gmail.com>
2018-06-30 21:36:35 +0300

    [ft] add Logout endpoint in user route

--- apiV2/routes/user.py
+++ apiV2/routes/user.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, jsonify, request
-from apiV2 import app
 from ..app.controllers import registration_controller, login_controller, rides_controller
 from flask_jwt_extended import jwt_required,  get_jwt_identity 
 from ..models.models import User,get_users, get_all_rides, get_ride_by_id
@@ -7,7 +6,7 @@ from utils import JSON_MIME_TYPE, json_response
 import datetime
 
 user_route = Blueprint("route_user",__name__)
-auth= Blueprint("authenctiactaion",__name__)
+auth = Blueprint("authenctiactaion",__name__)
 
 @auth.route('/signup', methods=['POST'])
 def register_user():
@@ -25,13 +24,19 @@ def login_user():
         return json_response(error, 400)
     return login_controller.login()
 
+@auth.route('/logout', methods=['DELETE'])
+@jwt_required
+def logout_user():
+    """user logout endpoint"""
+    return login_controller.logout()
+
+
 @user_route.route('/users', methods=['GET'])
 def get_all_users():
     """GET all users endpoint"""
     users = get_users()
     return jsonify({"all users": users}),200
-
-      
+   
 @user_route.route('/rides', methods=['POST'])
 @jwt_required
 def create_ride():
@@ -41,7 +46,6 @@ def create_ride():
         error = jsonify({'error': 'Invalid Content Type'})
         return json_response(error, 400)
     return rides_controller.create_new_ride_offer(user_id)
-
 
 @user_route.route('/rides', methods=['GET'])
 @jwt_required
@@ -55,27 +59,12 @@ def get_single_ride_offer(rideid):
     """GET single ride offer endpoint"""
     return jsonify({"Ride Offer:":get_ride_by_id(rideid)}),200
 
-
 @user_route.route('/rides/<rideid>/request', methods=['POST'])
 @jwt_required
 def join_ride_offer(ride):
     """Join ride endpoint"""
-
     return jsonify({"message":"Successfully joined ride"}),200
 
 
-# @user_route.route('/logout', methods=['DELETE'])
-# def logout_user():
-#     """user logout endpoint"""
-#     return jsonify({"message":"Successfully logged out"}),200
 
 
-
-
-# @user_route.route('/rides/joined',methods=['GET'])
-# def get_all_joined_ride_offer():
-#     """Get all Joined rides endpoint"""
-#     joined_rides = [ ride for ride in all_ride_offers if ride.get("joined",None) == "user"]
-#     if len(joined_rides) == 0:
-#         return jsonify({"message":"No joined Rides!"}),404
-#     return jsonify({"All joined rides":joined_rides}),200
