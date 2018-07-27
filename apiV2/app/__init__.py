18c9bc37025fa882953e01eb8431be85226f7e59
Derrick Kipchumba <dkip64@gmail.com>
2018-07-27 21:42:54 +0300

    [finishes #159372387] Enable CORS

--- apiV2/app/__init__.py
+++ apiV2/app/__init__.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from config import app_config
 from flask_jwt_extended  import  JWTManager
+from flask_cors import CORS
 from ..models.database import Database
 from ..routes.user import user_route ,auth
 
 db =  Database()
 
 def initialize_app(config_name="development"):
-    """initializing app in development mode"""
+    """initializing app """
     app = Flask(__name__, instance_relative_config=True) #creating our WSGI APP Object
     app.config.from_object(app_config[config_name])
-    db.__init__()
+    db.__init__() 
     jwt = JWTManager(app)
+    CORS(app)
     
     #register Blue_prints
     app.register_blueprint(user_route,url_prefix="/api/v2/users")
